3c539e77cd29f9e39e5323e7a0b3cdf341c720c6
samba914 <sambadiarra2000@gmail.com>
2021-01-30 16:29:58 +0100

    mise en place des choses à faire et déclaration des fonctions à implémenter

---
+++ grasp.py
@@ -0,0 +1,31 @@
+def construction(G):
+    #fonction à implémenter
+
+def LocalSearch(G):
+    #fonction à implementer
+
+
+#GRASP FONCTION
+def grasp(G,Tmax):
+    """
+    2: G∗ ← construction(G)
+    3: tstart ← time()
+    4: while time() − timestart < timemax do
+    5:      G' ← construction(G)
+    6:      G' ← Local Search(G')
+    7:      if s(G') < s(G∗) then
+    8:          G∗ ← G'
+    9:      end if
+    10: end while
+    11: return G∗ as output
+    12: end procedure
+    """
+
+
+
+
+
+if __name__ == '__main__':
+    #1. find a way to extract data from the input file
+    #2. call the grasp fonction
+    #3. write the solution in a output file
